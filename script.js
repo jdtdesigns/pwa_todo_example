bc55505b62de1d592179668cb644a9c7ef512a66
JD Tadlock <jt7903@gmail.com>
2024-02-15 09:00:44 -0500

    finished basic todo app

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+const addBtn = document.querySelector('#add-btn');
+
+function getTodos() {
+  const todoData = localStorage.getItem('todos');
+
+  return JSON.parse(todoData) || [];
+}
+
+function storeTodo(text) {
+  const todos = getTodos();
+
+  todos.push(text);
+
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function addTodo() {
+  const todoInput = document.querySelector('#todo-input')
+  const todoText = todoInput.value;
+
+  storeTodo(todoText);
+  alert('Your todo has been added successfully!');
+
+  todoInput.value = '';
+  outputTodos();
+}
+
+function outputTodos() {
+  const todos = getTodos();
+  const outputEl = document.querySelector('#output');
+
+  if (todos.length) {
+    outputEl.innerHTML = '';
+  }
+
+  todos.forEach(text => {
+    outputEl.insertAdjacentHTML('beforeend', `<p>${text}</p>`)
+  });
+}
+
+outputTodos();
+
+addBtn.addEventListener('click', addTodo);
+
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
